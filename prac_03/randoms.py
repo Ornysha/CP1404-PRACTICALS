8e1ffc14024da72263c87ef26f09cdaedef957e3
Ornysha <ornysha.biswas@my.jcu.edu.au>
2025-10-04 01:14:38 +1000

    Add random number examples

---
+++ prac_03/randoms.py
@@ -0,0 +1,13 @@
+# What did you see on line 1?
+# What was the smallest number you could have seen, what was the largest?
+# - smallest = 5 & highest = 20
+# What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest?
+# Could line 2 have produced a 4?
+# - smallest = 3 largest = 9. no, line 2 could not have produced a 4
+# What did you see on line 3?
+# What was the smallest number you could have seen, what was the largest?
+# - smallest = 2.5 largest = 5.5
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+import random
+print(random.randint(1,100))
